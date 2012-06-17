7d039f727a21d6a1d1ade34bb4fbf6f980bc4800
Daniel Narvaez <dwnarvaez@gmail.com>
2012-06-17 13:11:24 +0200

    Add a periodic build

--- schedulers.py
+++ schedulers.py
@@ -1,5 +1,6 @@
 from buildbot.schedulers.basic import SingleBranchScheduler
 from buildbot.schedulers.forcesched import ForceScheduler
+from buildbot.schedulers.timed import Periodic
 from buildbot.changes import filter
 
 def setup(c, config):
@@ -9,6 +10,11 @@ def setup(c, config):
     c["schedulers"].append(SingleBranchScheduler(name="all",
                                                  change_filter=change_filter,
                                                  builderNames=["build"]))
+
+    c["schedulers"].append(timed.Periodic(name="daily",
+                                          builderNames=["build"],
+                                          periodicBuildTimer=24*60*60))
+
     c["schedulers"].append(ForceScheduler(name="force",
                                           builderNames=["build"]))
 
