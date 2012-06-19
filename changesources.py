ce9e4e8f74dd6853d39482067e8bc450147439a7
Daniel Narvaez <dwnarvaez@gmail.com>
2012-06-19 12:34:18 +0200

    Fix typo

--- changesources.py
+++ changesources.py
@@ -6,7 +6,7 @@ def setup(c, config):
     c["change_source"] = []
 
     for repo in repos.get_all():
-        poller = GitPoller(repo.path,
+        poller = GitPoller(repo.url,
                            project="sugar",
                            workdir="gitpoller_work/%s" % repo.name)
         c["change_source"].append(poller)
