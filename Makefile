6802acaa7dc0a77d2ae3a4cae4a1b952f5849834
Daniel Narvaez <dwnarvaez@gmail.com>
2012-11-21 20:29:42 +0100

    Read the repos from json rather than duplicating

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+MODULES_REPO="git://git.sugarlabs.org/sugar-build/sugar-build.git"
+MODULES_SOURCE_PATH="config"
+MODULES_DEST_PATH=$(CURDIR)/modules
+
+MODULES=modules/sugar.json \
+        modules/activities.json
+
 SOURCES=master.cfg \
         builders.py \
         repos.py \
@@ -5,6 +12,8 @@ SOURCES=master.cfg \
         changesources.py \
         schedulers.py \
         statustargets.py \
+
+.PHONY: all sync pull-modules
 
 all:
 	@echo -e "Targets:\n"
@@ -14,7 +23,19 @@ sync.path:
 	@echo "Please create a sync.path file pointing to the master directory."
 	@exit 1
 
-sync: sync.path
-	@cp $(SOURCES) `cat sync.path`
-	@cp -n config.py `cat sync.path`
-	@echo "Sync done".
+sync: sync.path pull-modules
+	@DEST_PATH=`cat sync.path` && \
+	cp $(SOURCES) $$DEST_PATH && \
+	cp -n config.py $$DEST_PATH && \
+        mkdir -p $$DEST_PATH/modules && \
+        cp $(MODULES) $$DEST_PATH/modules && \
+	echo "Sync done".
+
+pull-modules:
+	@TMP_DIR=`mktemp -td sugar-build-XXXX` && \
+	git clone $(MODULES_REPO) $$TMP_DIR && \
+        mkdir -p $(MODULES_DEST_PATH) && \
+        cd $$TMP_DIR/$(MODULES_SOURCE_PATH) && \
+        cp $(MODULES) $(MODULES_DEST_PATH) && \
+	rm -rf $$TMP_DIR && \
+	echo "Modules updated."
--- repos.py
+++ repos.py
@@ -1,31 +1,16 @@
 class Repo:
-    def __init__(self, name, path, branch="master"):
+    def __init__(self, name, url, branch=None):
         self.name = name
         self.url = "git://git.sugarlabs.org/%s" % path
         self.branch = branch
 
-main_repo = Repo(name="sugar-build",
-                 path="sugar-build/sugar-build.git")
+        if self.branch is None:
+            self.branch = "master"
 
-sub_repos = [Repo(name="sugar",
-                  path="sugar/mainline.git"),
-             Repo(name="sugar-docs",
-                  path="sugar-docs/sugar-docs.git"),
-             Repo(name="sugar-base",
-                  path="sugar-base/mainline.git"),
-             Repo(name="sugar-toolkit",
-                  path="sugar-toolkit/mainline.git"),
-             Repo(name="sugar-datastore",
-                  path="sugar-datastore/mainline.git"),
-             Repo(name="sugar-artwork",
-                  path="sugar-artwork/mainline.git"),
-             Repo(name="sugar-toolkit-gtk3",
-	          path="sugar-toolkit-gtk3/sugar-toolkit-gtk3.git"),
-             Repo(name="chat",
-                  path="chat/mainline.git",
-                  branch="gtk3"),
-             Repo(name="browse",
-                  path="browse/mainline.git")]
+main_repo = Repo(name="sugar-build",
+                 path="git://git.sugarlabs.org/sugar-build/sugar-build.git")
+
+sub_repos = []
 
 def get_by_name(name):
     repos = [main_repo]
@@ -49,3 +34,12 @@ def get_main_repo():
 
 def get_sub_repos():
     return sub_repos
+
+def load_modules(path):
+    for module in json.load(open(path)):
+        sub_repos.append(Repo(name=module["name"],
+                              url=module["repo"],
+                              branch=module.get("branch", None)))
+
+load_modules("modules/sugar.json")
+load_modules("modules/activities.json")
