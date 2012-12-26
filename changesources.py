9ee40c4e40651c9ae3a3e3c04aeda318a30f4d99
Daniel Narvaez <dwnarvaez@gmail.com>
2012-12-26 01:15:11 +0100

    1 minute poll interval

--- changesources.py
+++ changesources.py
@@ -11,12 +11,14 @@ def setup(c, config):
     poller = GitPoller(main_repo.url,
                        project=main_repo.name,
                        category=category,
-                       branches=[main_repo.branch])
+                       branches=[main_repo.branch],
+                       pollinterval=60)
     c["change_source"].append(poller)
 
     for repo in repos.get_sub_repos():
         poller = GitPoller(repo.url,
                            project=main_repo.name,
                            category=category,
-                           branches=[repo.branch])
+                           branches=[repo.branch],
+                           pollinterval=60)
         c["change_source"].append(poller)
