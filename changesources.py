bbceef0bace1a93b2f777bfbdf573653f4fb59fa
Daniel Narvaez <dwnarvaez@gmail.com>
2013-01-01 17:41:27 -0500

    Remove obsolete category

--- changesources.py
+++ changesources.py
@@ -6,12 +6,10 @@ import repos
 def setup(c):
     c["change_source"] = []
 
-    category = "production"
-
     main_repo = repos.get_main_repo()
+
     poller = GitPoller(main_repo.url,
                        project=main_repo.name,
-                       category=category,
                        branches=[main_repo.branch],
                        pollinterval=60)
     c["change_source"].append(poller)
@@ -19,7 +17,6 @@ def setup(c):
     for repo in repos.get_sub_repos():
         poller = GitPoller(repo.url,
                            project=main_repo.name,
-                           category=category,
                            branches=[repo.branch],
                            pollinterval=60)
         c["change_source"].append(poller)
