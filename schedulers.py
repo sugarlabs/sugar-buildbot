ec710a1dc6273bae84c661a38b3d845bd5e086f8
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-25 16:29:15 +0200

    Add repo for osbuild

--- schedulers.py
+++ schedulers.py
@@ -20,7 +20,9 @@ def setup(c, config):
     all_builders.extend(full_builders)
 
     codebases = {"sugar-build": {"repository": config["repo"]},
-                 "osbuild": {}}
+                 "osbuild": {repository:
+                             "https://github.com/dnarvaez/osbuild.git"}}
+
     for repo in repos.get_sub_repos():
         codebases[repo.name] = {"repository": repo.url}
 
