0b707725e16ce26331c9821f44102447ed6c048a
Daniel Narvaez <dwnarvaez@gmail.com>
2013-06-16 02:57:37 +0200

    Move to #sugar

--- statustargets.py
+++ statustargets.py
@@ -22,7 +22,7 @@ def setup(c, config):
 
     c["status"].append(words.IRC(host="irc.freenode.net",
                                  nick="sugarbuildbot",
-                                 channels=["#sugar-buildbot"],
+                                 channels=["#sugar"],
                                  notify_events={"failureToSuccess": 1,
                                                 "failure": 1}))
 
