d880dec4e2699abc57db2615674811a0799483f8
Daniel Narvaez <dwnarvaez@gmail.com>
2013-07-11 23:56:18 +0200

    Maximum of one build per slave

--- slaves.py
+++ slaves.py
@@ -5,6 +5,7 @@ def setup(c, config):
     c["slaves"] = []
 
     for name, info in config["slaves"].items():
-        c["slaves"].append(BuildSlave(name, info["password"]))
+        c["slaves"].append(BuildSlave(name, info["password"],
+                                      max_builds=1))
 
     c["slavePortnum"] = config["slaves_port"]
