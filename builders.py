f68f75f956618ec2943338985b1ab8b123bf9ab7
Daniel Narvaez <dwnarvaez@gmail.com>
2014-04-05 13:12:26 +0100

    Pass list of sources

--- builders.py
+++ builders.py
@@ -15,12 +15,11 @@ class PullCommand(ShellCommand):
     def setBuild(self, build):
         ShellCommand.setBuild(self, build)
 
-        sources = {}
+        sources = []
         for source_stamp in build.getAllSourceStamps():
             if source_stamp.revision:
-                sources[source_stamp.codebase] = \
-                    {"repository": source_stamp.repository,
-                     "revision": source_stamp.revision}
+                sources.append({"repository": source_stamp.repository,
+                                "revision": source_stamp.revision})
 
         command = ["./osbuild", "pull"]
 
